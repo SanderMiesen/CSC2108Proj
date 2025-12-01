6da1087318f5beafa151a401c6c217ba89624402
SanderMiesen <sander.miesen@epfl.ch>
2025-11-30 19:03:43 -0500

    Revert "Added/deleted relevant file for custom PPO method evaluation"
    This reverts commit 29210ddf76edbe3ffc8bb6ebeadb68e76a0cc4f8.

--- nudge/env_paper.py
+++ nudge/env_paper.py
@@ -50,7 +50,7 @@ class NudgeBaseEnv(ABC):
 
     @staticmethod
     def from_name(name: str, **kwargs):
-        env_path = f"in/envs/{name}/env.py"
+        env_path = f"in/envs/{name}/env_paper.py"
         env_module = load_module(env_path)
         return env_module.NudgeEnv(**kwargs)
 
